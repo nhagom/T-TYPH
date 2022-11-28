70172cda6cd620a87eb0945631be5351ab64904c
nth.thanh23@gmail.com <nth.thanh23@gmail.com>
2022-11-29 00:21:59 +0700

    cập nhật mới box chat 2 _ Thanh

--- box chat/boxchat.js
+++ box chat/boxchat.js
@@ -1,8 +1,8 @@
 const chatButton = document.querySelector('.chatbox__button');
 const chatContent = document.querySelector('.chatbox__support');
 const icons = {
-    isClicked: '<img src="./images/icons/chatbox-icon.svg" />',
-    isNotClicked: '<img src="./images/icons/chatbox-icon.svg" />'
+    isClicked: '<img src="./boxchat_images/icons/chatbox-icon.png" />',
+    isNotClicked: '<img src="./boxchat_images/icons/chatbox-icon.png" />'
 }
 const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
 chatbox.display();
