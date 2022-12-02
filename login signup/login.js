8365291fc2bd4886dbb64f7be665115baf4c727c
tailee02 <dumathangtai@gmail.com>
2022-12-02 14:22:36 +0700

    logIn và gioiThieu

---
+++ login signup/login.js
@@ -0,0 +1,42 @@
+// var tabLinks = document.querySelectorAll(".tabLinks");
+// var tabContent = document.querySelectorAll(".tabContent")
+// tabLinks.forEach(function(el){
+//   el.addEventListener("click",openTabs);
+// })
+// function openTabs(el){
+//   var btn = el.currentTarget
+//   var electronic = btn.dataset.electronic
+//   tabContent.forEach(function(el){
+//     el.classList.remove("active");
+//   })
+//   tabLinks.forEach(function(el){
+//     el.classList.remove("active");
+//   })
+//   document.querySelector("#"+electronic).classList.add("active");
+//   btn.classList.add("active")
+// }
+
+// var tabLinks = document.querySelectorAll(".tabLinks");
+// var tabContent =document.querySelectorAll(".tabContent");
+
+// tabLinks.forEach(function(el) {
+//    el.addEventListener("click", openTabs);
+// });
+
+
+// function openTabs(el) {
+//    var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
+//    var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
+ 
+//    tabContent.forEach(function(el) {
+//       el.classList.remove("active");
+//    });
+
+//    tabLinks.forEach(function(el) {
+//       el.classList.remove("active");
+//    });
+
+//    document.querySelector("#" + electronic).classList.add("active");
+   
+//    btn.classList.add("active");
+// }
