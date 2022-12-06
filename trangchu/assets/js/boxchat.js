3def0c13b52edb93c5b0c15d93d9cdd415b6f58d
nth.thanh23@gmail.com <nth.thanh23@gmail.com>
2022-12-06 13:18:47 +0700

    fix box chat trang chủ_ HT

--- trangchu/assets/js/boxchat.js
+++ trangchu/assets/js/boxchat.js
@@ -2,7 +2,7 @@ const chatButton = document.querySelector('.chatbox__button');
 const chatContent = document.querySelector('.chatbox__support');
 const icons = {
     isClicked: '<img src="./icons/chatbox-icon.png" />',
-    isNotClicked: '<img src="./" />'
+    isNotClicked: '<img src="./icons/chatbox-icon.png" />'
 }
 const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
 chatbox.display();
